d9975a1ba2c4a103377e30e9d3a454da80b4b034
SirotkinD <50509618+sdtad@users.noreply.github.com>
2025-02-28 10:20:50 +0200

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+document.getElementById('body').addEventListener('click', function() {
+	let passiveAnimation = document.getElementById('passiveAnimation');
+	passiveAnimation.style.display = 'none';
+
+	let activeAnimation = document.getElementById('activeAnimation');
+	activeAnimation.style.display = 'block';
+	
+ })
+
+
